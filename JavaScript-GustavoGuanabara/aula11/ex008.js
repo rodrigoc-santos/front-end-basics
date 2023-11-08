8a225204001149dee396186ee00eee477b34737e
rodrigoc-santos <rodrigocoelho.20rsc@gmail.com>
2023-11-08 14:45:14 -0300

    Avançando nos estudos de JavaScript

---
+++ JavaScript-GustavoGuanabara/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 45.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 50) { // condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)    
+}
+console.log('Dirija sempre usando o cinto de segurança!')
