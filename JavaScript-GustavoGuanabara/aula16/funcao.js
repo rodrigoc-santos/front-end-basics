72b3a401926ec98d6ad1ec3818aaef3346daba33
rodrigoc-santos <rodrigocoelho.20rsc@gmail.com>
2023-11-21 17:00:50 -0300

    Functions

---
+++ JavaScript-GustavoGuanabara/aula16/funcao.js
@@ -0,0 +1,36 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+let res = parimpar(6)
+
+console.log(res)
+
+function soma (n1=0, n2=0) {  // variáveis opcionais pré-definidas
+    return n1 + n2
+}
+
+console.log(soma(1))
+
+// jogando uma função dentro de uma variável
+
+let x = function(y) {
+    return y*2
+}
+
+console.log(x(25))
+
+// RECRUSIVIDADE
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
